4fb65d06fcc5ab10681e158e64cbb66cfd054804
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2018-09-24 10:36:21 +0200

    Create modules.js

---
+++ static/js/modules.js
@@ -0,0 +1,5 @@
+module.exports = {
+   one : require('https://raw.githubusercontent.com/riccardotommasini/colorwave/master/static/js/jsonld-viz.js'),
+   two : require('https://d3js.org/d3.v5.min.js'),
+   /* some other modules you want */
+}
